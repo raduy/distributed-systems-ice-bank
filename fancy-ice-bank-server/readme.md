f71f00334bb0e42f589f6d116e46a6c526341221
raduy <raduj.lukasz@gmail.com>
2015-05-19 14:04:55 +0200

    Project structure scaffolding

---
+++ fancy-ice-bank-server/readme.md
@@ -0,0 +1,4 @@
+#Server setup: (on Ubuntu)
+1. Install *slice2java*
+$ sudo apt-get install ice35-translators 3.5.2
+
---
+++ fancy-ice-bank-server/src/main/java/pl/agh/sr/icebank/Hello.java
@@ -0,0 +1,7 @@
+package pl.agh.sr.icebank;
+
+/**
+ * @author Lukasz Raduj <raduj.lukasz@gmail.com>
+ */
+public class Hello {
+}
